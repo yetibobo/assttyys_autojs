9e7fa38920a12379921ad2e14723deac32b2c482
yetibobo <270833700@qq.com>
2025-07-17 17:16:23 +0800

    Update RoundButton.js

--- src/system/FloatButton/widget/RoundButton.js
+++ src/system/FloatButton/widget/RoundButton.js
@@ -8,14 +8,28 @@
  * @LastEditTime: 2021-04-19 12:19:40
  */
 /*eslint-disable */
-var RoundButton = (function () {
-    util.extend(RoundButton, ui.Widget);
 
+// 1. 使用立即执行函数(IIFE)包裹代码，创建闭包作用域避免污染全局
+var RoundButton = (function () {
+
+    // 2.  $util.extend(target, source)
+    // target {object}  目标构造函数
+    // source {object} 要继承的构造函数
+    // 将原型方法从一个构造函数继承到另一个构造函数。
+    // 类似于Node.js中的util.inherits
+    util.extend(RoundButton, ui.Widget);
+    
+    // 3. 定义RoundButton构造函数
     function RoundButton() {
+        
+        // 4. 调用父类构造函数，确保父类初始化逻辑被执行
         ui.Widget.call(this);
     }
-
+    
+    // 5. 在原型上添加render方法（重写父类方法）
     RoundButton.prototype.render = function () {
+        
+         // 6. 返回按钮的HTML字符串定义
         return "<img\
             padding='8'\
             layout_gravity='center_vertical'\
@@ -23,9 +37,13 @@ var RoundButton = (function () {
             backgroundTint='#FFFFFF'\
         />";
     }
-
+    
+    // 7. 将自定义组件注册到UI系统，注册名为"widget-RoundButton"
     ui.registerWidget("widget-RoundButton", RoundButton);
+    
+    // 8. 返回构造函数供外部使用
     return RoundButton;
-})();
+})();// 立即执行函数结束
 
+// 9. 通过CommonJS规范导出模块
 module.exports;
